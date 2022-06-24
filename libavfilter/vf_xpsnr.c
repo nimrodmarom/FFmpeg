784ed17649364a395ce1788eda27817347d479d9
nimrodmarom <nimrod.marom@gmail.com>
2022-06-24 19:15:34 +0300

    adding one more function to profiling, and removing short functions from profiling - fix 2

--- libavfilter/vf_xpsnr.c
+++ libavfilter/vf_xpsnr.c
@@ -492,8 +492,8 @@ static int getWSSE(AVFilterContext *ctx, int16_t **org, int16_t **orgM1, int16_t
     }
   } /* for c */
   // TODO: R&N Delete begin
-  gettimeofday(&end_calcSquarredError, 0);
-  av_log(ctx, AV_LOG_INFO, "******getWSSE: differnt: %lf ******\n", ((unsigned long long)((1000000 * end.end_calcSquarredError + end_calcSquarredError.tv_usec) - (1000000 * begin.tv_sec + begin.tv_usec)) / 1000000.0));
+  gettimeofday(&end, 0);
+  av_log(ctx, AV_LOG_INFO, "******getWSSE: differnt: %lf ******\n", ((unsigned long long)((1000000 * end.tv_sec + end.tv_usec) - (1000000 * begin.tv_sec + begin.tv_usec)) / 1000000.0));
   // TODO: R&N Delete end
   return 0;
 }
